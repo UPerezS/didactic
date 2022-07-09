837c50b88d53b30d65052fb7d769ca76fe46c8c5
UrielPS <cama8836@gmail.com>
2022-07-08 20:25:30 -0500

    c1

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdministradorComponent } from './components/administrador/administrador.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { InstructorComponent } from './components/instructor/instructor.component';
+import { Instructor1Component } from './components/instructor1/instructor1.component';
+import { Instructor2Component } from './components/instructor2/instructor2.component';
+import { Instructor3Component } from './components/instructor3/instructor3.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { NavInstructorComponent } from './components/nav-instructor/nav-instructor.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdministradorComponent,
+    AlumnosComponent,
+    InstructorComponent,
+    Instructor1Component,
+    Instructor2Component,
+    Instructor3Component,
+    HomeComponent,
+    NavegacionComponent,
+    PiePaginaComponent,
+    RegistroComponent,
+    NavInstructorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
